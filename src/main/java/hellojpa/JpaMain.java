f31a4e29f655622ea22d28d986aeb9605dccfafd
chaelynjang <chaelynjang@lotte.net>
2024-05-15 15:13:01 +0900

    영속성 컨텍스트 이해

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -21,17 +21,23 @@ public class JpaMain {
             // update
             findMember.setName("updatedHellooA");
             // em.remove(findMember); -> 삭제
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("HelloB");
-//            em.persist(member);
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloD");
+            // 영속 상태가 되는데 befor/after 주석 사이에 before와 after 주석 사이에 아무런 sql도 찍히지 않고 추후에 insert 문이 찍힌다!
+            System.out.println("==== BEFORE ====");
+            em.persist(member);
+            System.out.println("==== AFTER ====");
+            Member member101 = em.find(Member.class, 101L);
+            System.out.println("member101 = " + member101.getName());
+
             List<Member> resultList = em.createQuery("select m from Member m", Member.class)
                     // jpql은 페이징 처리도 가능
                     .setFirstResult(1)
                     .setMaxResults(10)
                     .getResultList();
-            for (Member member : resultList) {
-                System.out.println("member = " + member.getName());
+            for (Member memberEntity : resultList) {
+                System.out.println("member = " + memberEntity.getName());
             }
             tx.commit();
         } catch (Exception e) {
