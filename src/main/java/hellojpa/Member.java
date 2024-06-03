a551e1526c9db1fc2e78dcbd2e52eaf3f1157cd9
jcl <chaelynjang@lotte.net>
2024-06-03 11:40:17 +0900

    다양한 연관관계 매핑 설정

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,17 +1,28 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
 
     @ManyToOne
     @JoinColumn(name = "TEAM_ID")
     private Team team;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
 
     public Long getId() {
         return id;
